2f80702cc30b9db3477367653e31af2397feba94
zahidhasandipu <zahidhasandipu@gmail.com>
2021-08-10 23:30:38 -0700

    JS Refresher 01

---
+++ Javascript/scripts/01/basic.js
@@ -0,0 +1,164 @@
+// Practice from freeCodeCamp.org
+
+// // # Declare Variables
+
+// var myName = "Dipu"; //for whole program
+
+// myName = 8;
+
+// console.log(myName);
+
+// let ourName = "Free Code Camp"; //only within the scope where it's declared
+
+// const pi = 3.14; //can never change
+
+// // # Storing Values with the Assignment Operator
+
+// var a;
+// var b = 2;
+// console.log(a);
+// a = 7;
+// b = a;
+// console.log(a);
+
+// // # Initializing Variables with the Assignment Operator
+
+// var a = 9;
+
+// // # Uninitialized Variables
+
+// // Initialize these three variables
+
+// var a = 6;
+// var b = 12;
+// var c = "I am a";
+
+// //Do not change code below this line
+
+// a = a + 1;
+// b = b + 5;
+// c = c + " String";
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// // # Adding & Subtracting Numbers
+
+// var sum = 10 + 20;
+// console.log(sum);
+
+// var difference = 50 - 20;
+// console.log(difference);
+
+// // # Multiplication & Division
+
+// var product = 5 * 6;
+// console.log(product);
+
+// var quotient = 100 / 20;
+// console.log(quotient);
+
+// // # Increment & Decrement
+
+// var myVar = 45;
+// myVar = myVar + 1;
+// console.log(myVar);
+
+// var myVar = 45;
+// myVar = myVar - 1;
+// console.log(myVar);
+
+// // # Remainer Operator
+
+// var remainder;
+// remainder = 11 % 3;
+// console.log(remainder);
+
+// // # Augmented Addition
+
+// var a = 10;
+// var b = 15;
+// var c = 20;
+
+// a += 10;
+// b += 15;
+// c += 20;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// // # Declare String Variables
+
+// var firstName = "Zahid";
+// var lastName = " Dipu";
+
+// console.log(firstName+lastName);
+
+// // # Escaping Literal Quotes
+
+// var myStr = "I am a \"Double Quoted\" string inside \"Double Quotes\"";
+// console.log(myStr);
+
+// // # Quoting Strings with Single Quotes
+
+// var myStr = 'I am a \"Double Quoted\" string inside \"Double Quotes\"';
+// console.log(myStr);
+
+// // # Escape Sequences
+
+// var myStr = "FirstLine\n\SecondLine\nThirdLine";
+// console.log(myStr);
+
+// // # Constructing Strings with Variables
+
+// var ourName = " Free Code Camp";
+// var ourStr = "Hi, our name is" + ourName + ". How are you doing today?";
+
+// console.log(ourStr);
+
+// // # Find length of string
+
+// var lastNameLength = 0;
+// var lastName = "Dipu";
+// lastNameLength = lastName.length;
+// console.log(lastNameLength);
+
+// // # Bracket Notation
+
+// var firstLetterOfLastName = "";
+// var lastName = "Dipu";
+
+// firstLetterOfLastName = lastName[0];
+// console.log(firstLetterOfLastName);
+
+// // # String Immutability
+
+// var myStr = "Jello World";
+// myStr = "Hello World" //J only can be changed by this way
+
+// # Arrays
+
+// var myArray = ["Dipu", 25];
+// console.log(myArray);
+
+// // # Nested Arrays
+
+// var myArray = [
+//   ["FC Barcelona", 2020],
+//   ["PSG", 2021],
+// ];
+// console.log(myArray);
+
+// // # Access Array Data
+
+// var myArray = [10, 20, 20];
+// var myData = myArray[0];
+// console.log(myData);
+
+// // #Modify Array Data
+
+// var myArray = [40, 50, 60];
+// myArray[0] = 45;
+// console.log(myArray);
