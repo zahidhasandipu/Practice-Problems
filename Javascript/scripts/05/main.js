a5b97f106da1d0b2b42770e96741fc89a403dab3
zahidhasandipu <zahidhasandipu@gmail.com>
2021-08-13 14:09:10 -0700

    JS Refresher 05

--- Javascript/scripts/05/main.js
+++ Javascript/scripts/05/main.js
@@ -115,3 +115,29 @@
 // var myConcat = (arr1, arr2) => arr1.concat(arr2);
 
 // console.log(myConcat([1, 2], [3, 4, 5]));
+
+
+// # Wrtie higher order arrow functions (Need to practice more)
+
+// const realNumberArray = [4, 5.6, -9.8, 3.14, 43, 6, 8.34, -2];
+
+// const squareList = (arr) => {
+//   const squaredIntegers = arr
+//     .filter((num) => Number.isInteger(num) && num > 0)
+//     .map((x) => x * x);
+// };
+
+// const squaredIntegers = squareList(realNumberArray);
+
+// console.log(squaredIntegers);
+
+// //Higher order arrow functions
+
+// const increment = (function () {
+//   return function increment(number, value = 1) { 
+//     return number + value;
+//   };
+// })();
+
+// console.log(increment(5, 2));
+// console.log(increment(5));
