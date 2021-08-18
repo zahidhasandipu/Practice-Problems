ad224d6a06098011eee35c8d9b724374162adae7
zahidhasandipu <zahidhasandipu@gmail.com>
2021-08-18 14:16:56 -0700

    JS HW 02

--- JavaScript HW/02/main.js
+++ JavaScript HW/02/main.js
@@ -38,3 +38,25 @@ HINT: You might need to look into JavaScript sort function and also a few type c
 // console.log(maxValue);
 // console.log(medValue);
 // console.log(avgValue);
+
+/*2.      Write a function that takes different parameters and creates a person object. The parameters would be:
+-        Name
+-        Height
+-        Weight
+-        BMI (initially null)
+It would also contain a method to calculate BMI of the person if the BMI is currently null, and set the new BMI. And also, another function to compare BMI with another person.
+Now, from user’s take inputs (prompt function) and create 2 persons. Show their individual BMI. And also demonstrate the compare function. The compare function should output something similar to this: “(Name1)’s BMI is greater than (Name2)’s”. Also, consider the fact that both BMI can be the same.
+NOTE: You are restricted to use only Objects and Object methods. Avoid using any external function except the object constructor. */
+
+//NA
+
+/* 3. Create an array of student objects with around 5-6 students (hardcode it, no need to take user inputs), each student having the following attributes:
+-        Name
+-        Marks: Array
+The objects should not contain any object methods, only name and marks. Your task is to find the brightest student of them all based on average marks. Consider the fact that multiple students might have the same average marks, in that case all of them would be considered brightest.
+The outputs should be strictly like this:
+Alice is the brightest student of them all
+OR
+Alice, John and Bob are the brightest students
+Avoid code repetitions
+NOTE TO TA: Please make sure that the program works correctly both for single brightest and multiple brightest students.*/
