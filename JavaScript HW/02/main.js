d55800b6f78838807a1268aecdcf73cc8ef3fe54
zahidhasandipu <zahidhasandipu@gmail.com>
2021-08-17 17:24:44 -0700

    JS HW 02

---
+++ JavaScript HW/02/main.js
@@ -0,0 +1,40 @@
+/*Write a program that takes 5 number inputs (prompt function) from users and stores it in the array. NOTE: The program has to be error-proof. The user can input anything including Strings, Numbers, Special Characters etc. Your job would be to ignore invalid values (optionally show an alert warning that the value is not a valid number) and only accept 5 numbers.
+HINT: You might need to look into JavaScript sort function and also a few type checking functions
+*/
+
+// var userInput = [];
+
+// for (let i = 0; i < 5; i++) {
+//   firstInput = parseInt(prompt("Please enter 5 number here!"));
+//   if (isNaN(firstInput)) {
+//     alert("Hey, It's not a number!");
+//     i--;
+//   } else {
+//     userInput.push(firstInput);
+//   }
+// }
+// console.log(userInput);
+
+// // Print out the min, max, median and average of the array.
+
+// userInput.sort();
+// let minValue = userInput[0];
+// let maxValue = userInput[0];
+// let medValue = userInput[2];
+// let avgValue = userInput[0];
+
+// for (var i = 1; i < userInput.length; i++) {
+//   if (userInput[i] < minValue) {
+//     minValue = userInput[i];
+//   }
+//   if (userInput[i] > maxValue) {
+//     maxValue = userInput[i];
+//   }
+//   avgValue = avgValue + userInput[i];
+// }
+// avgValue = avgValue / userInput.length;
+
+// console.log(minValue);
+// console.log(maxValue);
+// console.log(medValue);
+// console.log(avgValue);
