fe2ecba68f8705144fb48ca6422ea537fd60e4a2
zahidhasandipu <zahidhasandipu@gmail.com>
2021-08-30 12:54:48 -0700

    00

---
+++ HackerRank Problems/Day 0/main.js
@@ -0,0 +1,79 @@
+// /**
+//  *   A line of code that prints "Hello, World!" on a new line is provided in the editor.
+//  *   Write a second line of code that prints the contents of 'parameterVariable' on a new line.
+//  *
+//  *	Parameter:
+//  *   parameterVariable - A string of text.
+//  **/
+// function greeting(parameterVariable) {
+//   // This line prints 'Hello, World!' to the console:
+//   console.log("Hello, World!");
+
+//   // Write a line of code that prints parameterVariable to stdout using console.log:
+//   console.log(parameterVariable);
+// }
+
+/**
+ *   The variables 'firstInteger', 'firstDecimal', and 'firstString' are declared for you -- do not modify them.
+ *   Print three lines:
+ *   1. The sum of 'firstInteger' and the Number representation of 'secondInteger'.
+ *   2. The sum of 'firstDecimal' and the Number representation of 'secondDecimal'.
+ *   3. The concatenation of 'firstString' and 'secondString' ('firstString' must be first).
+ *
+ *	Parameter(s):
+ *   secondInteger - The string representation of an integer.
+ *   secondDecimal - The string representation of a floating-point number.
+ *   secondString - A string consisting of one or more space-separated words.
+ **/
+// function performOperation(secondInteger, secondDecimal, secondString) {
+//   // Declare a variable named 'firstInteger' and initialize with integer value 4.
+//   const firstInteger = 4;
+
+//   // Declare a variable named 'firstDecimal' and initialize with floating-point value 4.0.
+//   const firstDecimal = 4.0;
+
+//   // Declare a variable named 'firstString' and initialize with the string "HackerRank".
+//   const firstString = "HackerRank ";
+
+//   // Write code that uses console.log to print the sum of the 'firstInteger' and 'secondInteger' (converted to a Number        type) on a new line.
+//   console.log(firstInteger + parseInt(secondInteger));
+
+//   // Write code that uses console.log to print the sum of 'firstDecimal' and 'secondDecimal' (converted to a Number            type) on a new line.
+//   console.log(firstDecimal + parseFloat(secondDecimal));
+
+//   // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
+//   console.log(firstString + secondString);
+// }
+
+// /**
+// *   Calculate the area of a rectangle.
+// *
+// *   length: The length of the rectangle.
+// *   width: The width of the rectangle.
+// *
+// *	Return a number denoting the rectangle's area.
+// **/
+// function getArea(length, width) {
+//     let area;
+//     // Write your code here
+//     area = length * width;
+
+//     return area;
+// }
+
+// /**
+// *   Calculate the perimeter of a rectangle.
+// *
+// *	length: The length of the rectangle.
+// *   width: The width of the rectangle.
+// *
+// *	Return a number denoting the perimeter of a rectangle.
+// **/
+// function getPerimeter(length, width) {
+//     let perimeter;
+//     // Write your code here
+//     perimeter = 2 * (length + width)
+
+//     return perimeter;
+// }
+
