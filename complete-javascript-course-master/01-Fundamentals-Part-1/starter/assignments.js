ef18ae47f6ede98df8a3907e49acad9badc20461
zahidhasandipu <zahidhasandipu@gmail.com>
2021-09-07 00:54:48 -0700

    values, variables, data types

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,27 @@
+/*1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2. Log their values to the console 
+*/
+
+// let country = "Bangladesh";
+// let continent = "Asia";
+// let population = "163,000,000";
+
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+/* 1. Declare a variable called 'isIsland' and set its value according to your
+country. The variable should hold a Boolean value. Also declare a variable
+'language', but don't assign it any value yet
+2. Log the types of 'isIsland', 'population', 'country' and 'language'
+to the console
+*/
+
+// let isIsland = false;
+// let language;
+
+// console.log(isIsland);
+// console.log(population);
+// console.log(country);
+// console.log(language);
---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,6 @@
+// let js = "Awesome";
+
+// console.log(40 + 8 + 23 - 10);
+
+// let firstName = "Hasan";
+// console.log(firstName);
